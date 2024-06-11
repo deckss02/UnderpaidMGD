de616309486e4f29218fc138d69252754902003b
MonoKrow <monokrow22@gmail.com>
2024-06-12 04:19:37 +0800

    Added Follower
    Still need to maintain reference between script and gameobjects. Keeps breaking references every playtest.

--- Assets/Scripts/Player/PlayerSwap Button.cs
+++ Assets/Scripts/Player/PlayerSwap Button.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 
 public class PlayerSwapButton : MonoBehaviour
 {
-    public PlayerController controller;
+    public PlayerControllera controller;
     public GameObject Player; //Make a direct reference to the Player
+    public GameObject FollowingPlayer;
 
     public LevelManager levelManager;
 
@@ -18,15 +19,18 @@ public class PlayerSwapButton : MonoBehaviour
     public Sprite Rhe;
     public RuntimeAnimatorController p1Anim; //Switches the player's animation controller during gameplay.
     public RuntimeAnimatorController p2Anim;
+    public RuntimeAnimatorController p1AnimF; //Switches the following characer's animator during gameplay.
+    public RuntimeAnimatorController p2AnimF; 
+
 
     public int ChangeNumber = 0; //This integer details which Character is in use.
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<PlayerController>();
         Player = GetComponent<GameObject>();
-
+        controller = GetComponent<PlayerControllera>();
+        FollowingPlayer = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -44,6 +48,7 @@ public class PlayerSwapButton : MonoBehaviour
             ChangeNumber = 0;
         }
 
+        //Switch Statement allows smooth back-&-forth swapping.
         switch (ChangeNumber)
         {
             case 0:
@@ -59,11 +64,17 @@ public class PlayerSwapButton : MonoBehaviour
     {
         Player.GetComponent<SpriteRenderer>().sprite = Corn;
         Player.GetComponent<Animator>().runtimeAnimatorController = p1Anim;
+
+        FollowingPlayer.GetComponent<SpriteRenderer>().sprite = Rhe;
+        FollowingPlayer.GetComponent<Animator>().runtimeAnimatorController = p2AnimF; ;
     }
 
     void Rhea()
     {
         Player.GetComponent<SpriteRenderer>().sprite = Rhe;
         Player.GetComponent<Animator>().runtimeAnimatorController = p2Anim;
+
+        FollowingPlayer.GetComponent<SpriteRenderer>().sprite = Corn;
+        FollowingPlayer.GetComponent<Animator>().runtimeAnimatorController = p1AnimF; ;
     }    
 }
