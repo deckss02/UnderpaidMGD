3fc916b2376151ac40b6f30dabb874c4d5035e66
Weba194 <christinechan194@gmail.com>
2024-07-25 19:57:28 +0800

    Revert "Update MGD.unity"
    This reverts commit be2cf6c819f5afbe3a2d0d3b707fc1d3c0ccf88f.

--- Assets/Scripts/Others/Skip.cs
+++ Assets/Scripts/Others/Skip.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Skip : MonoBehaviour
 {
-    //public GameObject dialogueBox;
-
-    public PlayableDirector PlayableDirector;
-    public float UpdateTime;
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +17,12 @@ public class Skip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateTime -= Time.deltaTime;
+        
     }
 
-    public void SkipDialogue()
+    public void SkipDialogue(GameObject dialogueBox)
     {
-        UpdateTime = 0;
-        if (UpdateTime  <= 0)
-        {
-            SceneManager.LoadScene("MGD");
-        }
-        //dialogueBox.SetActive(false);
+        dialogueBox.SetActive(false);
     }
 
 }
